59f713a0be76970348b770c8156cd4c3c7046546
UltimateAnjali <anjali21295@gmail.com>
2017-07-12 17:09:21 -0400

    just

--- app/src/main/java/com/example/staffonechristian/quotesmaster/MainActivity.java
+++ app/src/main/java/com/example/staffonechristian/quotesmaster/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.staffonechristian.quotesmaster;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
