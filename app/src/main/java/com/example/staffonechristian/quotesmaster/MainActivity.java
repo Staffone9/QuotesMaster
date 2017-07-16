991f6ce9a31c52a9fe395197feac108a83528211
staffonechristian <christianstaffone@gmail.com>
2017-07-16 16:15:15 -0400

    Login Done Only UI remaining

--- app/src/main/java/com/example/staffonechristian/quotesmaster/MainActivity.java
+++ app/src/main/java/com/example/staffonechristian/quotesmaster/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.staffonechristian.quotesmaster;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,14 +28,28 @@ public class MainActivity extends AppCompatActivity {
     QuoteData mData;
     ProgressBar pBar;
     DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Intent intent = new Intent(getApplicationContext(),Category.class);
-//        startActivity(intent);
 
+
+        //code by JamesBond007
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null)
+                {
+                    Intent intent = new Intent(getApplicationContext(),SignIn.class);
+                    startActivity(intent);
+                }
+            }
+        };
         //Code by Anjali
 
         pBar = (ProgressBar)findViewById(R.id.progress_bar);
@@ -51,6 +67,12 @@ public class MainActivity extends AppCompatActivity {
         prepareQuotes();
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     private void prepareQuotes(){
